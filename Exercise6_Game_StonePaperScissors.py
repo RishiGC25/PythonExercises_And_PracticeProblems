e4cec3e0d57ca64fa354b773c7d6d72c6294dc83
Rishi <rishigc25n@gmail.com>
2021-04-16 23:46:59 +0530

    Made Code Look Better

--- Exercise6_Game_StonePaperScissors.py
+++ Exercise6_Game_StonePaperScissors.py
@@ -3,9 +3,11 @@ import sys  # Importing sys
 from pygame import mixer  # Importing mixer from pygame
 
 lst = ["S", "P", "X"]  # A list of the options: Stone, Paper and Scissors from which a random generation of choice occurs
+
 attempts = 0  # In the beginning, the number of attempts have to be zero
 compu = 0  # In the beginning, the score of computer has to be zero
 user = 0  # In the beginning, the score of user has to be zero
+
 print("Stone, Paper or Scissors?")
 print("Enter:")
 
@@ -104,6 +106,8 @@ while True:
             print("Please enter a valid letter and try again!")
         attempts += 1
 
+
+
     if attempts >= 10:  # When all (10) attempts get over
         
         if user > compu:  # When User scores more than Computer/CPU
@@ -126,10 +130,14 @@ while True:
 
         again = input()  # Taking User input
         PlayAgain = again.upper()
+        
         if PlayAgain == "Y":
             print("OKAY!\nGET READY")
+
         else:
             print("Bye")
             sys.exit(0)
+
+
     else:
         print("Bye")
